12d20b38b61549ae635782e1617454cf39dd9561
mina-etml <pk60ftv@eduvaud.ch>
2025-11-19 08:46:16 +0100

    New Course: Router - Pages as a propos parameter (to test it : http://localhost:5173/events/3)

---
+++ src/components/Events.vue
@@ -0,0 +1,6 @@
+<script setup>
+defineProps(['page'])
+</script>
+<template>
+  <h1>You are on page {{ page }}</h1>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
 import EventDetailsView from '../views/EventDetailsView.vue'
+import Events from '../components/Events.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
       name: 'event-details',
       props: true,
       component: EventDetailsView,
+    },
+    {
+      path: '/events/:page',
+      component: Events,
+      props: true,
     },
   ],
 })
